4946fc62a22ffcce08dc39e3ceee013c673dd9d1
José Mateus <mateusjoseph@gmail.com>
2025-07-23 13:03:47 -0300

    atualizando as variáveis msgVerm, msgAmar e msgVerd

--- script.js
+++ script.js
@@ -18,27 +18,27 @@ function ligarVermelho(){
     document.getElementById("vermelho").classList.add("verm");
 
     //fazendo publish no tópico, (broker)
-    const msg = new Paho.MQTT.Message("");
-    msg.destinationName = "senai661/led/vermelho/on"
-    clientWeb.send(msg)
+    const msgVerm = new Paho.MQTT.Message("");
+    msgVerm.destinationName = "senai661/led/vermelho/on"
+    clientWeb.send(msgVerm);
 }
 
 function ligarAmarelo(){
     document.getElementById("amarelo").classList.add("amar");
 
     //fazendo publish no tópico, (broker)
-    const msg = new Paho.MQTT.Message("");
-    msg.destinationName = "senai661/led/amarelo/on"
-    clientWeb.send(msg)
+    const msgAmar = new Paho.MQTT.Message("");
+    msgAmar.destinationName = "senai661/led/amarelo/on"
+    clientWeb.send(msgAmar)
 }
 
 function ligarVerde(){
     document.getElementById("verde").classList.add("verd");
 
     //fazendo publish no tópico, (broker)
-    const msg = new Paho.MQTT.Message("");
-    msg.destinationName = "senai661/led/verde/on"
-    clientWeb.send(msg)
+    const msgVerd = new Paho.MQTT.Message("");
+    msgVerd.destinationName = "senai661/led/verde/on"
+    clientWeb.send(msgVerd)
 }
 
 function ligarAutomatico(){
@@ -48,6 +48,5 @@ function ligarAutomatico(){
 function desligar(){
     document.getElementById("vermelho").classList.remove("verm");
     document.getElementById("amarelo").classList.remove("amar");
-    document.getElementById("verde").classList.remove("verd");
-    
+    document.getElementById("verde").classList.remove("verd");    
 }
